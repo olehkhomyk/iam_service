dd8a4f469e4d98a0b80463e0d6b07ee9895646f0
et37599 <et37599@jostens.com>
2025-12-04 20:18:31 +0200

    Added migration file (V1__init.sql)

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE posts
+(
+    id      BIGSERIAL PRIMARY KEY,
+    title   VARCHAR(255) NOT NULL,
+    content TEXT         NOT NULL,
+    created TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    likes   INTEGER      NOT NULL DEFAULT 0,
+    UNIQUE (title)
+);
+
+INSERT INTO posts (title, content, created, likes) VALUES
+    ('First Post', 'This is my first post in this application', CURRENT_TIMESTAMP, 10),
+    ('Second Post', 'This is another post in the application', CURRENT_TIMESTAMP, 6);
+
