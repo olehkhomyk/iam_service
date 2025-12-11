3e9ca1af10977d777d964b7a78253a891f97a55c
olehkhomyk <oleh.khomyk@gmail.com>
2025-12-11 23:24:37 +0200

    Added exceptions handler

--- src/main/java/com/post_hub/iam_service/model/constants/ApiConstants.java
+++ src/main/java/com/post_hub/iam_service/model/constants/ApiConstants.java
@@ -4,6 +4,12 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ApiConstants {
+public final class ApiConstants {
+
 	public static final String UNDEFINED = "undefined";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String BREAK_LINE = "\n";
+	public static final String TIME_ZONE_PACKAGE_NAME = "java.time.zone";
+
 }
