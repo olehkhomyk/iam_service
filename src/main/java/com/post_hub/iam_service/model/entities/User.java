06379612d73135eaa4a0f2736fa2b575d19126f7
olehkhomyk <oleh.khomyk@gmail.com>
2025-12-19 21:27:39 +0200

    Updated db, added new users table added entity

---
+++ src/main/java/com/post_hub/iam_service/model/entities/User.java
@@ -0,0 +1,53 @@
+package com.post_hub.iam_service.model.entities;
+
+import com.post_hub.iam_service.model.enums.RegistrationStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Size(max = 30)
+	@Column(nullable = false, length = 30, unique = true)
+	private String username;
+
+	@Size(max = 80)
+	@Column(nullable = false, length = 80)
+	private String password;
+
+	@Size(max = 50)
+	@Column(unique = true, length = 50)
+	private String email;
+
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime created;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private LocalDateTime updated;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "registration_status", nullable = false)
+	private RegistrationStatus registrationStatus;
+
+	@Column(name = "last_login")
+	private LocalDateTime lastLogin;
+
+	@Column(nullable = false)
+	private boolean deleted = false;
+}
