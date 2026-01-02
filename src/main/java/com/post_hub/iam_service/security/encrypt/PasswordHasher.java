158d4edeb95b4ebabf645bc60d1d450e80ebce70
et37599 <et37599@jostens.com>
2026-01-02 22:26:26 +0200

    Security: Updated users with hashed passwords.

---
+++ src/main/java/com/post_hub/iam_service/security/encrypt/PasswordHasher.java
@@ -0,0 +1,17 @@
+package com.post_hub.iam_service.security.encrypt;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordHasher {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+				String first_password = encoder.encode("Test1111!");
+				String second_password = encoder.encode("Test2222!");
+				String third_password = encoder.encode("Test3333!");
+
+				System.out.println("first_password :" + first_password);
+				System.out.println("second_password :" + second_password);
+				System.out.println("third_password :" + third_password);
+    }
+}
